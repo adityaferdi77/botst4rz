07e9d152edbe4a55defa98ede47712907edbe204
Dani-ID <73680515+Dani-ID@users.noreply.github.com>
2020-11-09 12:58:26 +0700

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const menu = require("./lib/menu.js");
 const donate = require("./lib/donate.js");
 const info = require("./lib/info.js");
 //
-const BotName = 'DANTOD BOT 🤖'; // Nama Bot Whatsapp
+const BotName = 'DANTOD'; // Nama Bot Whatsapp
 const instagramlu = 'https://instagram.com/dani.rknt'; // Nama Instagramlu cok
 const whatsapplu = '0812-3497-5117'; // Nomor whatsapplu cok
 const kapanbotaktif = '24 Jam'; // Kapan bot lu aktif
