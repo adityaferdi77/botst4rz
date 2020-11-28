b67609301e6086acdd96ad8d2661643d20c5f0b3
Dani-ID <73680515+Dani-ID@users.noreply.github.com>
2020-11-28 13:55:51 +0700

    Update index.js

--- index.js
+++ index.js
@@ -509,10 +509,6 @@ conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.tex
 else if (text == 'Makasi'){
 conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.text);
 }
-else if (text == '#grupofficialbot'){
-conn.sendMessage(id, ' *GRUP* : TERMUX WHATSAPP BOT
-*LINK GRUP* : https://chat.whatsapp.com/JOoPF87XRI4LBIhjkSQvCc' ,MessageType.text);
-}
 else if (text == `#hentai`){
 conn.sendMessage(id , `*Selamat datang Di fitur hentai*\n\n*-------------------------------------------------*\n ⃝⃔Nekopoi\nhttp://nekopoi.care\n\n ⃝⃔Animeidhentai\nhttp://animeidhentai.com\n\n ⃝⃔kisshentai\nhttp://kisshentai.com\n\n ⃝⃔h-anime\nhttp://h-anime.com\n\n ⃝⃔Hentaipulse\nhttp://hentaipulse.com\n\n ⃝⃔hentaimama\nhttp://hentaimama.com\n\n ⃝⃔miniopai\nhttp://minioppai.org\n\n ⃝⃔yandex\nhttps://yandex.com\n\nJika Mau Memakai salah satu website silahkan Gunakan *VPN* terlebih dahulu` ,MessageType.text)
 conn.sendMessage(id, `*Dosa Di Tanggung Sendiri Owner Tidak Ikut Campur*` ,MessageType.text);
