7e685a37471d3e241e7c216a24bb2356732c5d19
Dani-ID <73680515+Dani-ID@users.noreply.github.com>
2020-11-28 13:53:31 +0700

    Update index.js

--- index.js
+++ index.js
@@ -510,8 +510,8 @@ else if (text == 'Makasi'){
 conn.sendMessage(id, 'Sama sama, semoga harimu menyenangkan :)' ,MessageType.text);
 }
 else if (text == '#grupofficialbot'){
-conn.sendMessage(id, '*GRUP* : VYN X DANTOD
-*LINK GRUP* : https://chat.whatsapp.com/I37dKx8gXobLrZsI6WqSt6' ,MessageType.text);
+conn.sendMessage(id, ' *GRUP* : TERMUX WHATSAPP BOT
+*LINK GRUP* : https://chat.whatsapp.com/JOoPF87XRI4LBIhjkSQvCc' ,MessageType.text);
 }
 else if (text == `#hentai`){
 conn.sendMessage(id , `*Selamat datang Di fitur hentai*\n\n*-------------------------------------------------*\n ⃝⃔Nekopoi\nhttp://nekopoi.care\n\n ⃝⃔Animeidhentai\nhttp://animeidhentai.com\n\n ⃝⃔kisshentai\nhttp://kisshentai.com\n\n ⃝⃔h-anime\nhttp://h-anime.com\n\n ⃝⃔Hentaipulse\nhttp://hentaipulse.com\n\n ⃝⃔hentaimama\nhttp://hentaimama.com\n\n ⃝⃔miniopai\nhttp://minioppai.org\n\n ⃝⃔yandex\nhttps://yandex.com\n\nJika Mau Memakai salah satu website silahkan Gunakan *VPN* terlebih dahulu` ,MessageType.text)
